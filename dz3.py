1d90c468bf07561254bd8f61c66ead0cff9ab92a
Mihail_Makhinya <mihail_makhinya@MacBook-Air-Mihail-Makhinya.local>
2025-04-20 20:26:47 +0300

    Initial commit

---
+++ dz3.py
@@ -0,0 +1,31 @@
+#a = {"Сегодня":["выучить python"]}
+#b = {"Завтра":["разработать todo-приложение"]}
+#c = {"Послезавтра":["разработать телеграм бота"]}
+#print(a["Сегодня"])
+#print(b["Завтра"])
+#print(c["Послезавтра"])
+
+
+####################
+slovar = {}
+
+date = input("Введите дату:")
+task = input("Введите задачу:")
+slovar[date] = task
+
+print(date, task)
+print(slovar)
+
+date = input("Введите дату:")
+task = input("Введите задачу:")
+slovar[date] = task
+
+print(date, task)
+print(slovar)
+
+date = input("Введите дату:")
+task = input("Введите задачу:")
+slovar[date] = task
+
+print(date, task)
+print(slovar)
